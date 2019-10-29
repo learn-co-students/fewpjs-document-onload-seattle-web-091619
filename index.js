419ca6976d468113788fbdf570921f9ea47bc7d1
Packmanager9 <ColeJDitzler@gmail.com>
2019-10-29 14:50:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let holder = document.getElementById('text')
+    holder.textContent = "This is really cool!"
+  });
